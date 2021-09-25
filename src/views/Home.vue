d1d1701c5b43984ca7b41f642bc95c6873e04673
gdlyeabkov <gdlyeabkov@gmail.com>
2021-09-25 09:20:25 +0300

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,106 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="hui-header">
+      <p class="hui-active hui-header-link">
+        huiCSS
+      </p>
+      <div class="hui-header-link-container">
+        <p class="hui-header-link">
+          Примеры
+        </p>
+        <p class="hui-header-link">
+          Документы
+        </p>
+        <p class="hui-header-link">
+          Скачать
+        </p>
+      </div>
+    </div>
+    <div class="hui-aside">
+      <p class="hui-aside-link">Примеры</p>
+      <p class="hui-aside-link">Кнопки</p>
+      <p class="hui-aside-link">Хедеры</p>
+      <p class="hui-aside-link">Стили</p>
+    </div>
+    <div class="hui-main">
+      <button class="hui-button  hui-dark">Нажми меня</button>
+      <button class="hui-button  hui-light">Нажми меня</button>
+      <button class="hui-button  hui-error">Нажми меня</button>
+      <button class="hui-button  hui-happy">Нажми меня</button>
+      <button class="hui-button hui-round-button hui-happy"></button>
+      <button class="hui-button hui-round-button hui-light"></button>
+      <button class="hui-button hui-round-button hui-dark"></button>
+      <button class="hui-button hui-round-button hui-error"></button>
+      <p>asd</p>
+    </div>
+
+    <div class="hui-footer">
+      <div class="hui-footer-links-bar">
+        <div class="hui-footer-link-bar">
+          <p class="hui-footer-link hui-footer-link-header">
+            Ссылки
+          </p>
+          <p class="hui-footer-link">
+            Домой
+          </p>
+          <p class="hui-footer-link">
+            Документы
+          </p>
+          <p class="hui-footer-link">
+            Примеры
+          </p>
+        </div>
+        <div class="hui-footer-link-bar">
+          <p class="hui-footer-link hui-footer-link-header">
+            Руководства
+          </p>
+          <p class="hui-footer-link">
+            Начиная
+          </p>
+          <p class="hui-footer-link">
+            Шаблон
+          </p>
+        </div>
+        <div class="hui-footer-link-bar">
+          <p class="hui-footer-link hui-footer-link-header">
+            Проекты
+          </p>
+          <p class="hui-footer-link">
+            huiCSS 1
+          </p>
+          <p class="hui-footer-link">
+            huiCSS 2
+          </p>
+          <p class="hui-footer-link">
+            huiCSS 3
+          </p>
+        </div>
+        <div class="hui-footer-link-bar">
+          <p class="hui-footer-link hui-footer-link-header">
+            Сообщества
+          </p>
+          <p class="hui-footer-link">
+            Проблемы
+          </p>
+          <p class="hui-footer-link">
+            Stack overflow
+          </p>
+          <p class="hui-footer-link">
+            Обсуждения
+          </p>
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+  
   }
 }
 </script>
